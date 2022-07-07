30a714b4af67ffcd37f4e4639e8d2458aa07b201
Hao Nan <87043260+haonan-vk@users.noreply.github.com>
2022-07-07 10:19:45 -0700

    Fix empty json body request with content-type: application/json header (#4)

--- flask_pydantic_spec/flask_backend.py
+++ flask_pydantic_spec/flask_backend.py
@@ -135,7 +135,7 @@ class FlaskBackend:
                 raw_body = gzip.decompress(request.stream.read()).decode(encoding="utf-8")
                 parsed_body = json.loads(raw_body)
             else:
-                parsed_body = request.get_json() or {}
+                parsed_body = {} if request.get_data() == b"" else request.get_json(force=True)
         elif request.content_type and "multipart/form-data" in request.content_type:
             parsed_body = request.form or {}
         else:

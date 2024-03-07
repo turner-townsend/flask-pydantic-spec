8510fc975029cf039b1783f8e28624cbb723a6d0
Jack Keane <jack.keane@verkada.com>
2024-03-07 11:48:23 -0800

    black fix

--- flask_pydantic_spec/spec.py
+++ flask_pydantic_spec/spec.py
@@ -297,7 +297,11 @@ class FlaskPydanticSpec:
             "info": {
                 **self.config.INFO,
                 **{
-                    "title": category_titles.get(category, self.config.TITLE) if category else self.config.TITLE,
+                    "title": (
+                        category_titles.get(category, self.config.TITLE)
+                        if category
+                        else self.config.TITLE
+                    ),
                     "version": self.config.VERSION,
                 },
             },
